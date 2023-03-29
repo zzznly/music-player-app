596abcda94138ca35d48d76a20fda736c5679267
zzznly <zzznly@gmail.com>
2023-03-29 14:36:00 +0900

    refactor. 플레이리스트 상세페이지, SongList 컴포넌트

--- src/Pages/PlaylistDetail/index.tsx
+++ src/Pages/PlaylistDetail/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 // components
-import SearchResultSongList from "../../components/Player/PlayerBody/Search/SearchResultSongList";
+import SongList from "../../components/Player/PlayerBody/Search/SongList";
 
 // styles
 import "./style.scss";
@@ -9,6 +9,7 @@ import { Color } from "color-thief-react";
 
 // react-query
 import {
+  useCategoryPlaylists,
   usePlaylistDetail,
   usePlaylistTracks,
 } from "../../logics/queries/useQueries";
@@ -16,35 +17,15 @@ import {
 // types
 import { PlaylistDetailRes } from "../../types/playlist";
 import { TrackItem } from "../../types/tracks";
+import { useLocation } from "react-router-dom";
 
 export default function PlaylistDetail(): JSX.Element {
-  // 배경 색상 밝기에 따른 텍스트 색상 설정
-  const setTextColor = (hexColor: string): string => {
-    const color = hexColor?.slice(1); // 색상 앞의 # 제거
-    const rgb = parseInt(color, 16); // rrggbb를 10진수로 변환
-    const red = (rgb >> 16) & 0xff; // red 추출
-    const green = (rgb >> 8) & 0xff; // green 추출
-    const blue = (rgb >> 0) & 0xff; // blue 추출
-
-    const luma: number = 0.222 * red + 0.707 * green + 0.071 * blue;
-
-    return luma > 128 ? "#000" : "#fff";
-  };
-
+  const location = useLocation();
   const playlistParams = {
-    playlist_id: window.location.search.slice(4),
+    playlist_id: location.search.slice(4),
   };
+
   const [playlistTracks, setPlaylistTracks] = useState<TrackItem[]>([]);
-  usePlaylistTracks(playlistParams, {
-    onSuccess: ({ data }) => {
-      console.log(data?.items);
-
-      let arr: any[] = [];
-      data?.items.forEach((item: { track: any }) => arr.push(item.track));
-      setPlaylistTracks(arr);
-    },
-  });
-
   const [playlistDetail, setPlaylistDetail] = useState<PlaylistDetailRes>({
     collaborative: false,
     description: "",
@@ -84,13 +65,46 @@ export default function PlaylistDetail(): JSX.Element {
     type: "",
     uri: "",
   });
-  usePlaylistDetail(playlistParams, {
+
+  usePlaylistTracks(playlistParams, {
     onSuccess: ({ data }) => {
-      console.log(data);
-      setPlaylistDetail(data);
+      let arr: any[] = [];
+      data?.items.forEach((item: { track: any }) => arr.push(item.track));
+      setPlaylistTracks(arr);
     },
+    enabled: !!location.search.includes("?id"),
   });
 
+  usePlaylistDetail(playlistParams, {
+    onSuccess: ({ data }) => {
+      setPlaylistDetail(data);
+    },
+    enabled: !!location.search.includes("?id"),
+  });
+
+  useCategoryPlaylists(
+    { category_id: window.location.search.slice(13) },
+    {
+      onSuccess: ({ data }) => {
+        console.log(data);
+      },
+    }
+  );
+
+  // 배경 색상 밝기에 따른 텍스트 색상 지정
+  const setTextColor = (hexColor: string): string => {
+    const color = hexColor?.slice(1); // 색상 앞의 # 제거
+    const rgb = parseInt(color, 16); // rrggbb를 10진수로 변환
+    const red = (rgb >> 16) & 0xff; // red 추출
+    const green = (rgb >> 8) & 0xff; // green 추출
+    const blue = (rgb >> 0) & 0xff; // blue 추출
+
+    const luma: number = 0.222 * red + 0.707 * green + 0.071 * blue;
+
+    return luma > 128 ? "#000" : "#fff";
+  };
+
+  // 플레이리스트 총 재생시간 계산
   const getTotalDurationTime = (tracks: TrackItem[]): string => {
     const totalSeconds = tracks.reduce(
       (acc, item) => Math.floor(acc + item.duration_ms / 1000),
@@ -100,9 +114,10 @@ export default function PlaylistDetail(): JSX.Element {
     const hh = Math.floor(totalMinutes / 60);
     const mm = totalMinutes % 60;
 
-    return `약 ${hh}시간 ${mm}분`;
+    return `약 ${hh}${hh && "시간"} ${mm}${mm && "분"}`;
   };
 
+  // 숫자 세자리마다 콤마 삽입
   const setComma = (value: number): string => {
     return Number(value).toLocaleString();
   };
@@ -126,6 +141,7 @@ export default function PlaylistDetail(): JSX.Element {
                       color: setTextColor(data),
                     }}
                   >
+                    <div className={"playlist-detail__head-filter"}></div>
                     <div className={"playlist-detail__album"}>
                       <img
                         className={"playlist-detail__album-image"}
@@ -165,12 +181,35 @@ export default function PlaylistDetail(): JSX.Element {
             }}
           </Color>
           <div className="playlist-detail__controller-bar">
-            <button>play</button>
-            <button>like</button>
+            <button className="button--play" aria-hidden="true">
+              <svg
+                role="img"
+                height="24"
+                width="24"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+                data-encore-id="icon"
+              >
+                <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path>
+              </svg>
+            </button>{" "}
+            <button className="button--like">
+              <svg
+                role="img"
+                height="32"
+                width="32"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+                data-encore-id="icon"
+                fill="#fff"
+              >
+                <path d="M5.21 1.57a6.757 6.757 0 0 1 6.708 1.545.124.124 0 0 0 .165 0 6.741 6.741 0 0 1 5.715-1.78l.004.001a6.802 6.802 0 0 1 5.571 5.376v.003a6.689 6.689 0 0 1-1.49 5.655l-7.954 9.48a2.518 2.518 0 0 1-3.857 0L2.12 12.37A6.683 6.683 0 0 1 .627 6.714 6.757 6.757 0 0 1 5.21 1.57zm3.12 1.803a4.757 4.757 0 0 0-5.74 3.725l-.001.002a4.684 4.684 0 0 0 1.049 3.969l.009.01 7.958 9.485a.518.518 0 0 0 .79 0l7.968-9.495a4.688 4.688 0 0 0 1.049-3.965 4.803 4.803 0 0 0-3.931-3.794 4.74 4.74 0 0 0-4.023 1.256l-.008.008a2.123 2.123 0 0 1-2.9 0l-.007-.007a4.757 4.757 0 0 0-2.214-1.194z"></path>
+              </svg>
+            </button>
             <button>more</button>
           </div>
           <div className={"playlist-detail__content"}>
-            <SearchResultSongList searchResult={playlistTracks} />
+            <SongList searchResult={playlistTracks} />
           </div>
         </>
       ) : (
