e45fce52543fa86418ab90804443fbaa29530613
zzznly <zzznly@gmail.com>
2023-04-05 23:43:29 +0900

    feat. 로그인페이지 생성

---
+++ src/Pages/Login/index.tsx
@@ -0,0 +1,12 @@
+import "./style.scss";
+export default function Login(): JSX.Element {
+  return (
+    <div className="login">
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+      <a href="/">LOGIN TO SPOTIFY</a>
+    </div>
+  );
+}
