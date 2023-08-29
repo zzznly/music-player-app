5a9ac4d0d869204ee1ddbe4755eb3ed22b1bf62b
zzznly <zzznly@gmail.com>
2023-08-29 20:19:39 +0900

    style. Home 스타일 수정

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -9,6 +9,9 @@ import {
 
 // components
 import ListSection from "../../components/organisms/ListSection";
+import { useNewReleases } from "@service/Playlist/usePlaylist";
+import { useEffect } from "react";
+import PlaylistService from "@service/Playlist/PlaylistService";
 
 const Section = ({ id, name = "" }: CategoriesItem): JSX.Element => {
   const { data: { playlists: { items = [] } = {} } = {} } =
@@ -40,7 +43,18 @@ const Section = ({ id, name = "" }: CategoriesItem): JSX.Element => {
 };
 
 export default function Home(): JSX.Element {
-  const { data: { categories: { items = [] } = {} } = {} } = useCategories();
+  const { data: { categories: { items: categoryItems = [] } = {} } = {} } =
+    useCategories();
+
+  const {
+    data: {
+      // @ts-ignore
+      albums: { items: newReleaseItems } = {},
+    } = {},
+  } = useNewReleases();
+
+  console.log(1313, newReleaseItems);
+
   return (
     <div className={"wrap"}>
       <div className="playlist">
@@ -48,7 +62,27 @@ export default function Home(): JSX.Element {
           <h2>Explore</h2>
         </div>
         <div className="playlist__content">
-          {items.map((item: CategoriesItem) => (
+          <div className="section section--new-release">
+            <div className="section-wrap">
+              {newReleaseItems?.slice(3, 8).map((item: any) => (
+                <div
+                  className="section__item"
+                  style={{
+                    backgroundImage: `url(${item?.images[0].url}), linear-gradient(to top, rgba(77, 77, 86, 0.231372549), rgba(77, 77, 86, 0))`,
+                    backgroundSize: "cover",
+                    backgroundBlendMode: "multiply",
+                  }}
+                >
+                  <p className="section__item-name">{item?.name}</p>
+                  <p className="section__item-artist">
+                    {item?.artists[0].name}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {categoryItems.map((item: CategoriesItem) => (
             <Section key={item.id} {...item} />
           ))}
         </div>
