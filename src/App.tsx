10bdfd6e3865f7901775089172f6e6b6b6b85617
zzznly <zzznly@gmail.com>
2023-09-10 10:23:50 +0900

    불필요 요소 삭제, 주석

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Route, Routes } from "react-router-dom";
 
 // layouts
 import MainLayout from "./components/templates/MainLayout";
-import AuthLayout from "./components/templates/AuthLayout";
+// import AuthLayout from "./components/templates/AuthLayout";
 
 // components
 import Explore from "@pages/Explore";
@@ -32,7 +32,7 @@ export default function App(): React.ReactElement {
               <Route path=":category" element={<SearchCategory />} />
             </Route>
           </Route>
-          <Route path="/detail/:type" element={<DetailPage />} />
+          {/* <Route path="/detail/:type" element={<DetailPage />} /> */}
         </Route>
         {/* login 페이지가 불필요함. OAuth2.0을 사용할 것이므로 스포티파이 로그인 페이지 계정으로 연결해주면 됨 */}
         {/* <Route path="/login" element={<AuthLayout />}>
--- src/test/test.ts
+++
@@ -1,6 +0,0 @@
-export // card component test
-// jest, react-testing-library
-// 1. 썸네일, 노래명, 가수명이 순차적으로 표시되어야함
-// 2. 썸네일 클릭시 ㅇㅇㅇ으로 이동되어야 함
-// 3. 마우스 hover했을때 썸네일이 확대되어서 보여야함. 추가적으로 카드컴포넌트의 border값이 하얀색으로 보여야함.
- {};
