baf7a5c8ff426936ecc5e5a067c5330fe1ab3d76
zzznly <zzznly@gmail.com>
2023-09-11 19:48:06 +0900

    .env 환경변수 세팅

--- @types/type.d.ts
+++ @types/type.d.ts
@@ -1,6 +1,8 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    REACT_APP_SPOTIFY_CLIENT_ID: string;
-    REACT_APP_SPOTIFY_CLIENT_SECRET: string;
+    REACT_APP_CLIENT_ID: string;
+    REACT_APP_CLIENT_SECRET: string;
+    REACT_APP_AUTHORIZE_URL: string;
+    REACT_APP_REDIRECT_URL: string;
   }
 }
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -17,8 +17,8 @@ export const removeAuthToken = () => {
 export const getSpotifyAuthUrl = () => {
   const params = {
     response_type: "token",
-    client_id: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
-    redirect_uri: "http://localhost:5005",
+    client_id: process.env.REACT_APP_CLIENT_ID,
+    redirect_uri: process.env.REACT_APP_REDIRECT_URL,
     scope: [
       "user-read-currently-playing",
       "user-read-recently-played",
@@ -32,13 +32,13 @@ export const getSpotifyAuthUrl = () => {
     show_dialog: "true",
   };
 
-  return `https://accounts.spotify.com/authorize?${new URLSearchParams(
+  return `${process.env.REACT_APP_AUTHORIZE_URL}?${new URLSearchParams(
     params
   ).toString()}`;
 };
 
 export const redirectToLogin = ({
-  redirect_uri = "http://localhost:5005",
+  redirect_uri = process.env.REACT_APP_REDIRECT_URL,
   // redirect_uri = "http://zzznlyawsbucket.s3-website.ap-northeast-2.amazonaws.com",
   scope = [
     "user-read-currently-playing",
@@ -57,9 +57,9 @@ export const redirectToLogin = ({
   show_dialog?: boolean;
 } = {}) => {
   window.location.replace(
-    `https://accounts.spotify.com/authorize?${new URLSearchParams({
+    `${process.env.REACT_APP_AUTHORIZE_URL}?${new URLSearchParams({
       response_type: "token",
-      client_id: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
+      client_id: process.env.REACT_APP_CLIENT_ID,
       redirect_uri,
       scope: scope.join("%20"),
       show_dialog: String(show_dialog),
